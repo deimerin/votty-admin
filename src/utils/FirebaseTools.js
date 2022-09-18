11cc49949e560ec7e714e168f39f94b06fd81758
deimerin <deimer.castillo@correo.uis.edu.co>
2022-09-18 16:00:26 -0500

    deploy test

--- src/utils/FirebaseTools.js
+++ src/utils/FirebaseTools.js
@@ -5,19 +5,19 @@ import { getAuth } from "firebase/auth";
 
 // default databaseURL
 const firebaseConfig = {
-  apiKey: process.env.REACT_API_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_API_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_API_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_API_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_API_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_API_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_API_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-const ballotURL = process.env.REACT_API_FIREBASE_DATABASE_BALLOT
-const verfURL = process.env.REACT_API_FIREBASE_DATABASE_VERF
-const votURL = process.env.REACT_API_FIREBASE_DATABASE_VOT
-const candURL = process.env.REACT_API_FIREBASE_DATABASE_CAND
+const ballotURL = process.env.REACT_APP_FIREBASE_DATABASE_BALLOT
+const verfURL = process.env.REACT_APP_FIREBASE_DATABASE_VERF
+const votURL = process.env.REACT_APP_FIREBASE_DATABASE_VOT
+const candURL = process.env.REACT_APP_FIREBASE_DATABASE_CAND
 
 // Initialize Firebase
 // https://firebase.google.com/docs/database/usage/sharding
@@ -42,4 +42,4 @@ export const verfDB = getDatabase(verf);
 export const votDB = getDatabase(vot);
 export const candDB = getDatabase(cand);
 
-const private_key = 'asdas'
+const private_key = process.env.REACT_APP_FIREBASE_PRIVATE_KEY
