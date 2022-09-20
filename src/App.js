01c15e98d7a161cc9cb5eec50cbefc951bf29129
deimerin <deimer.castillo@correo.uis.edu.co>
2022-09-20 00:08:22 -0500

    fixed typo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import SignInSide from './pages/SignIn';
-import Dashboard from './pages/main';
-import { Route, Routes } from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext';
-import ProtectedRoute from './components/ProtectedRoute';
+import React from "react";
+import SignInSide from "./pages/SignIn";
+import Dashboard from "./pages/main";
+import { Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // #3734a8
 // var buf = crypto.randomBytes(16).toString('Base64');
@@ -11,17 +11,21 @@ import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-  <>
-<AuthContextProvider>
-
-  <Routes>
-    <Route path='/' element={<SignInSide />} />
-    <Route path='/main' element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />       
-  </Routes>
-
-</AuthContextProvider>
-
-  </>
+    <>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<SignInSide />} />
+          <Route
+            path="/main"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
+    </>
   );
 }
 
